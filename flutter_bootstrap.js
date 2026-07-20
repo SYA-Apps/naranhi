1bf408556e0fb915507f8d3339e3bb61cea1d427
SYA <sya@noreply.invalid>
2026-07-20 18:15:31 +0900

    build: e5ccf08 시점의 웹 미리보기

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "456826741" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2049977188" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
